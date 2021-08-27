51cbc57ef38fbf73c7bca6a487bd90f476dac099
Matej Sima <matej.sima@stove-labs.com>
2021-08-27 22:30:48 +0200

    Auction winner calculation formatting

--- utils/auction.ts
+++ utils/auction.ts
@@ -4,7 +4,7 @@ import { Bid, Parachain } from '../generated/model';
 import { ensure } from "./ensure"
 import { ensureParachain } from './parachain';
 import { find, times, findKey, partial, isEqual } from 'lodash';
-import linearScale  from 'simple-linear-scale';
+import linearScale from 'simple-linear-scale';
 
 export const ensureBid = async (
     store: DatabaseManager,
@@ -42,89 +42,38 @@ const leasePeriodsPerSlot = 8;
  * 
  * https://github.com/paritytech/polkadot/blob/9fc3088f9e8dae5eaf062503fcefbb75a548c016/runtime/common/slot_range_helper/src/lib.rs#L279
  */
-
 export type IndexedBids = { [key: string]: Bid };
 export type SlotRange = {
     leasePeriodStart: BN,
     leasePeriodEnd: BN,
 }
 
-// pub enum SlotRange {
-// 	ZeroZero,		0
-// 	ZeroOne,		1
-// 	ZeroTwo,		2
-// 	ZeroThree,		3
-// 	ZeroFour,		4
-// 	ZeroFive,		5
-// 	ZeroSix,		6
-// 	ZeroSeven,		7
-// 	OneOne,			8
-// 	OneTwo,			9
-// 	OneThree,		10
-// 	OneFour,		11
-// 	OneFive,		12
-// 	OneSix,			13
-// 	OneSeven,		14
-// 	TwoTwo,			15
-// 	TwoThree,		16
-// 	TwoFour,		17
-// 	TwoFive,		18
-// 	TwoSix,			19
-// 	TwoSeven,		20
-// 	ThreeThree,		21
-// 	ThreeFour,		22
-// 	ThreeFive,		23
-// 	ThreeSix,		24
-// 	ThreeSeven,		25
-// 	FourFour,		26
-// 	FourFive,		27
-// 	FourSix,		28
-// 	FourSeven,		29
-// 	FiveFive,		30
-// 	FiveSix,		31
-// 	FiveSeven,		32
-// 	SixSix,			33
-// 	SixSeven,		34
-// 	SevenSeven,		35
-// }
-
 const slotRangePairing = (() => {
     const rangeIndexes: { [key: string]: string } = {};
-    let index = 0;    
+    let index = 0;
     times(8).forEach(i => {
         times(8).forEach(j => {
             if (i > j) return;
-            rangeIndexes[`${i}-${j}`] = `${index}`;        
+            rangeIndexes[`${i}-${j}`] = `${index}`;
             index++;
         })
     })
     return rangeIndexes;
 })();
 
-
 export const slotRangeScale = linearScale([13, 20], [0, 7]);
 
-console.log(slotRangePairing);
 export const slotRangeIndexToLength = (slotRangeIndex: string) => {
     const minimalSlotRange = findKey(slotRangePairing, partial(isEqual, slotRangeIndex));
     const slotRange = minimalSlotRange?.split('-') as string[];
     const slotRangeLength = (new BN(slotRange[1])).sub((new BN(slotRange[0]))).add(new BN(1))
-    console.log('slotRangeIndexToLength', slotRange, slotRangeLength.toString());
     return slotRangeLength;
 }
 
 export const minimizeSlotRange = ({
     leasePeriodStart,
     leasePeriodEnd
-}: SlotRange) => {
-    //leasePeriodEnd.sub(leasePeriodStart).abs().add(new BN(1))
-    // return new BN(`${leasePeriodStart}${leasePeriodEnd}`)
-    // const minimalSlotRange = `${slotRangeScale(leasePeriodStart)}-${slotRangeScale(leasePeriodEnd)}`;
-    const minimalSlotRange = slotRangePairing[`${leasePeriodStart}-${leasePeriodEnd}`];
-    // console.log('minimizing slot range', `${leasePeriodStart}-${leasePeriodEnd}`, minimalSlotRange);
-    return minimalSlotRange;
-}
-
+}: SlotRange) => slotRangePairing[`${leasePeriodStart}-${leasePeriodEnd}`]
 
 export const bidsIntoRangeIndexes = (bids: Bid[]) => {
     const bidsWithIndexes: IndexedBids = {};
@@ -142,7 +91,6 @@ export const bestBidForRangeIndex = (
     slotRangeIndex: string
 ): BN | undefined => {
     const bid = indexedBids[slotRangeIndex];
-
     if (!bid) return;
 
     const slotLength = slotRangeIndexToLength(slotRangeIndex);
@@ -157,12 +105,10 @@ export const determineWinningBids = async (
 ): Promise<Bid[]> => {
     /**
      * Transform the currently in-db-indexed bids, into a object
-     * that uses slot range lengths as indexes, in order to be consumed
-     * by the winner calculation algorhytm
+     * that uses unique slot range combinations as indexes, in order to be consumed
+     * by the winner calculation algorithm
      */
     const indexedBids: IndexedBids = bidsIntoRangeIndexes(currentBids);
-
-    // console.log('indexedBids', indexedBids);
 
     const winningBids = (() => {
         const bestWinnersEndingAt: { [key: string]: any } = {}
@@ -174,7 +120,7 @@ export const determineWinningBids = async (
             };
             const slotRangeIndex = minimizeSlotRange(slotRange);
             const bid = bestBidForRangeIndex(indexedBids, slotRangeIndex);
-            // console.log('bid', bid?.toNumber());
+
             if (bid) {
                 bestWinnersEndingAt[i] = {
                     ranges: [slotRange],
@@ -189,46 +135,33 @@ export const determineWinningBids = async (
                 };
                 const slotRangeIndex = minimizeSlotRange(slotRange);
                 let bid = bestBidForRangeIndex(indexedBids, slotRangeIndex);
-                console.log('bid', bid?.toNumber());
 
                 // those two for loops above cover all slot range combinations
                 // now its time to choose the highest bids
-
                 if (bid) {
                     bid = bid.add(
                         bestWinnersEndingAt[j]?.bid
                         || new BN(0)
-                    )
-
-                    console.log('fail',bid.toNumber(), bestWinnersEndingAt[i]?.bid);
-
+                    );
 
                     if (bid.gt(bestWinnersEndingAt[i]?.bid || new BN(0))) {
-
-                        console.log('fail2',bid, bestWinnersEndingAt[i]?.bid);
                         bestWinnersEndingAt[i] = {
                             ranges: (bestWinnersEndingAt[j]?.ranges || []).concat(slotRange),
                             bid
                         }
                     }
-
                 } else {
-                    if (
-                        (bestWinnersEndingAt[j]?.bid || new BN(0)).gt(
-                            (bestWinnersEndingAt[i]?.bid || new BN(0))
-                        )
-                    ) {
+                    const shouldReplaceBestWinners = (bestWinnersEndingAt[j]?.bid || new BN(0))
+                        .gt((bestWinnersEndingAt[i]?.bid || new BN(0)))
+
+                    if (shouldReplaceBestWinners) {
                         bestWinnersEndingAt[i] = { ...bestWinnersEndingAt[j] };
                     }
                 }
             }
         }
 
-        
         const winningRanges = bestWinnersEndingAt[leasePeriodsPerSlot - 1]?.ranges;
-
-        // winningRanges.forEach((range: any) => console.log('range', range.leasePeriodStart.toString(), range.leasePeriodEnd.toString()))
-
         const winningBids = winningRanges
             .map((range: SlotRange) => {
                 const slotRangeIndex = minimizeSlotRange(range)
@@ -237,9 +170,8 @@ export const determineWinningBids = async (
             })
             .filter((winner: SlotRange) => winner);
 
-
         return winningBids;
-    })()
+    })();
 
     return winningBids;
 }
