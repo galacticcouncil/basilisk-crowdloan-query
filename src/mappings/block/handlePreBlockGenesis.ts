643fbe68af9b92805303e92f19379cd26dc1f2f0
dexterslabor <dexterslabor@users.noreply.github.com>
2021-12-27 16:40:08 +0100

    feat: add 2nd auctions batch migration data

--- src/mappings/block/handlePreBlockGenesis.ts
+++ src/mappings/block/handlePreBlockGenesis.ts
@@ -1,9 +1,21 @@
 import { ensureChronicle } from "../../utils/chronicle";
 import { StoreContext } from '@subsquid/hydra-common'
+import { ensureCrowdloan, updateCrowdloanFunds } from "../../utils/crowdloan";
+import { updateParachainFundsPledged } from "../../utils/parachain";
+import batch2auction from "./preBlockGenesis.json"
 
 const handlePreBlockGenesis = async ({
     store
 }: StoreContext) => {
+    const migrationPromises = batch2auction.map(async parachain => {
+        // ensure we have a crowdloan to assign to the contribution
+        let crowdloan = await ensureCrowdloan(store, String(parachain.paraId));
+        // account the current contribution towards the crowdloan raised funds
+        await updateCrowdloanFunds(store, crowdloan, BigInt(parachain.raised)),
+        await updateParachainFundsPledged(store, String(parachain.paraId));
+    })
+    await Promise.all(migrationPromises);
+
     await ensureChronicle(store);
 }
 export default handlePreBlockGenesis;
